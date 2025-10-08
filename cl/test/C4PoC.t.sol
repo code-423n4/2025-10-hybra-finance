e84e7eb887f587609ed1c16a4177ae64d1c52ce9
CleanAmirHosseinDev <amirbuissene1352@proton.me>
2025-10-08 23:05:38 +0330

    Fix incorrect vote weight calculation in VoterV3

--- cl/test/C4PoC.t.sol
+++ cl/test/C4PoC.t.sol
@@ -1,49 +1,80 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.7.6;
-pragma abicoder v2;
+pragma solidity 0.8.13;
 
 import "forge-std/Test.sol";
 
 import {C4PoCTestbed} from "./C4PoCTestbed.t.sol";
-
-/**
- * @title C4PoC
- * @notice A complete deployment of the CL contract suite
- * @dev All available variables are showcased in the commented
- *      storage slots of the contract.
- */
-
-// Deployer
-// address public deployer;
-
-// Token addresses
-// address public USDC;
-// address public USDT;
-// address public DAI;
-// address public WETH;
-
-// Deployment addresses
-// CLFactory public poolFactory;
-// CLPool public poolImplementation;
-// NonfungiblePositionManager public nonfungiblePositionManager;
-// NonfungibleTokenPositionDescriptor public nftPositionDescriptor;
-// SwapRouter public swapRouter;
-// QuoterV2 public quoter;
-// DynamicSwapFeeModule public swapFeeModule;
-// CustomUnstakedFeeModule public unstakedFeeModule;
-// CustomProtocolFeeModule public protocolFeeModule;
-
-// Configurational Variables
-// address public team;
-// address public poolFactoryOwner;
-// address public feeManager;
-// string public nftName;
-// string public nftSymbol;
+import "./helpers/MockERC20.sol";
 
 contract C4PoC is C4PoCTestbed {
     function setUp() public override {
         super.setUp();
     }
 
-    function test_submissionValidity() external {}
+    function test_submissionValidity() external {
+        // Create two mock ERC20 tokens
+        MockERC20 token1 = new MockERC20("Token1", "TKN1", 18);
+        MockERC20 token2 = new MockERC20("Token2", "TKN2", 18);
+        MockERC20 token3 = new MockERC20("Token3", "TKN3", 18);
+
+        // Mint some tokens for the user
+        token1.mint(address(this), 1_000_000e18);
+        token2.mint(address(this), 1_000_000e18);
+        token3.mint(address(this), 1_000_000e18);
+
+        // Whitelist tokens
+        vm.startPrank(permissionsRegistry.hybraTeamMultisig());
+        tokenHandler.addToken(address(token1), true);
+        tokenHandler.addToken(address(token2), true);
+        tokenHandler.addToken(address(token3), true);
+        vm.stopPrank();
+
+        // Create two pools
+        address pool1 = thenaFiFactory.createPair(
+            address(token1),
+            address(token2)
+        );
+        address pool2 = thenaFiFactory.createPair(
+            address(token1),
+            address(token3)
+        );
+
+        // Create gauges for the pools
+        vm.startPrank(permissionsRegistry.hybraTeamMultisig());
+        address gauge1 = gaugeManager.createGauge(pool1, 0);
+        address gauge2 = gaugeManager.createGauge(pool2, 0);
+        vm.stopPrank();
+
+        // Kill the first gauge
+        vm.startPrank(permissionsRegistry.hybraMultisig());
+        gaugeManager.killGauge(gauge1);
+        vm.stopPrank();
+
+        // Create a lock
+        hybr.approve(address(votingEscrow), 1_000e18);
+        uint256 tokenId = votingEscrow.create_lock(1_000e18, 86400 * 365 * 4);
+
+        // Vote
+        address[] memory pools = new address[](2);
+        pools[0] = pool1;
+        pools[1] = pool2;
+
+        uint256[] memory weights = new uint256[](2);
+        weights[0] = 5000;
+        weights[1] = 5000;
+
+        vm.warp(block.timestamp + 86400 * 8); // Move to the next epoch
+        voter.vote(tokenId, pools, weights);
+
+        // Check the vote weight
+        uint256 totalWeightForPool2 = voter.weights(pool2);
+        uint256 expectedWeight = votingEscrow.balanceOfNFT(tokenId) / 2;
+
+        // The weight for the alive gauge should be half of the total voting power
+        assertEq(
+            totalWeightForPool2,
+            expectedWeight,
+            "Vote weight should be distributed correctly"
+        );
+    }
 }
--- ve33/contracts/VoterV3.sol
+++ ve33/contracts/VoterV3.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import './libraries/Math.sol';
-import './interfaces/IBribe.sol';
-import './interfaces/IERC20.sol';
-import './interfaces/IPairInfo.sol';
-import './interfaces/IPairFactory.sol';
-import './interfaces/IVotingEscrow.sol';
-import './interfaces/IGaugeManager.sol';
-import './interfaces/IPermissionsRegistry.sol';
-import './interfaces/ITokenHandler.sol';
+import "./libraries/Math.sol";
+import "./interfaces/IBribe.sol";
+import "./interfaces/IERC20.sol";
+import "./interfaces/IPairInfo.sol";
+import "./interfaces/IPairFactory.sol";
+import "./interfaces/IVotingEscrow.sol";
+import "./interfaces/IGaugeManager.sol";
+import "./interfaces/IPermissionsRegistry.sol";
+import "./interfaces/ITokenHandler.sol";
 import {HybraTimeLibrary} from "./libraries/HybraTimeLibrary.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
@@ -19,24 +19,24 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
-    address public _ve;                                         // the ve token that governs these contracts
-    address internal base;                                      // $the token
-    address public permissionRegistry;                          // registry to check accesses
-    address public tokenHandler;                     
+    address public _ve; // the ve token that governs these contracts
+    address internal base; // $the token
+    address public permissionRegistry; // registry to check accesses
+    address public tokenHandler;
     uint256 public maxVotingNum;
     uint public EPOCH_DURATION;
     uint256 internal constant MIN_VOTING_NUM = 10;
     IGaugeManager public gaugeManager;
-    
-    mapping(uint256 => mapping(address => uint256)) public votes;  // nft      => pool     => votes
-    mapping(uint256 => address[]) public poolVote;                 // nft      => pools
+
+    mapping(uint256 => mapping(address => uint256)) public votes; // nft      => pool     => votes
+    mapping(uint256 => address[]) public poolVote; // nft      => pools
 
     mapping(address => uint256) public weights;
     uint256 public totalWeight;
     mapping(uint256 => uint256) public usedWeights;
 
-    mapping(uint256 => uint256) public lastVoted;                     // nft      => timestamp of last vote (this is shifted to thursday of that epoc)
-    mapping(uint256 => uint256) public lastVotedTimestamp;            // nft      => timestamp of last vote
+    mapping(uint256 => uint256) public lastVoted; // nft      => timestamp of last vote (this is shifted to thursday of that epoc)
+    mapping(uint256 => uint256) public lastVotedTimestamp; // nft      => timestamp of last vote
 
     event Voted(address indexed voter, uint256 tokenId, uint256 weight);
     event Abstained(uint256 tokenId, uint256 weight);
@@ -61,7 +61,7 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         maxVotingNum = 30;
         EPOCH_DURATION = HybraTimeLibrary.WEEK;
     }
- 
+
     /* -----------------------------------------------------------------------------
     --------------------------------------------------------------------------------
     --------------------------------------------------------------------------------
@@ -71,17 +71,35 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     ----------------------------------------------------------------------------- */
 
     modifier VoterAdmin() {
-        require(IPermissionsRegistry(permissionRegistry).hasRole("VOTER_ADMIN",msg.sender), 'VOTER_ADMIN');
+        require(
+            IPermissionsRegistry(permissionRegistry).hasRole(
+                "VOTER_ADMIN",
+                msg.sender
+            ),
+            "VOTER_ADMIN"
+        );
         _;
     }
 
     modifier Governance() {
-        require(IPermissionsRegistry(permissionRegistry).hasRole("GOVERNANCE",msg.sender), 'GOVERNANCE');
+        require(
+            IPermissionsRegistry(permissionRegistry).hasRole(
+                "GOVERNANCE",
+                msg.sender
+            ),
+            "GOVERNANCE"
+        );
         _;
     }
 
     modifier GenesisManager() {
-        require(IPermissionsRegistry(permissionRegistry).hasRole("GENESIS_MANAGER", msg.sender), 'GENESIS_MANAGER');
+        require(
+            IPermissionsRegistry(permissionRegistry).hasRole(
+                "GENESIS_MANAGER",
+                msg.sender
+            ),
+            "GENESIS_MANAGER"
+        );
         _;
     }
 
@@ -93,10 +111,10 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     --------------------------------------------------------------------------------
     ----------------------------------------------------------------------------- */
 
- 
-
     /// @notice Set a new PermissionRegistry
-    function setPermissionsRegistry(address _permissionRegistry) external VoterAdmin {
+    function setPermissionsRegistry(
+        address _permissionRegistry
+    ) external VoterAdmin {
         require(_permissionRegistry.code.length > 0, "CODELEN");
         require(_permissionRegistry != address(0), "ZA");
         emit SetPermissionRegistry(permissionRegistry, _permissionRegistry);
@@ -104,10 +122,9 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     }
 
     function setMaxVotingNum(uint256 _maxVotingNum) external VoterAdmin {
-        require (_maxVotingNum >= MIN_VOTING_NUM, "LOW_VOTE");
+        require(_maxVotingNum >= MIN_VOTING_NUM, "LOW_VOTE");
         maxVotingNum = _maxVotingNum;
     }
-
 
     /* -----------------------------------------------------------------------------
     --------------------------------------------------------------------------------
@@ -117,10 +134,14 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     --------------------------------------------------------------------------------
     ----------------------------------------------------------------------------- */
 
-    
     /// @notice Reset the votes of a given TokenID
-    function reset(uint256 _tokenId) external onlyNewEpoch(_tokenId) nonReentrant {
-        require(IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, _tokenId), "NAO");
+    function reset(
+        uint256 _tokenId
+    ) external onlyNewEpoch(_tokenId) nonReentrant {
+        require(
+            IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, _tokenId),
+            "NAO"
+        );
         _reset(_tokenId);
         IVotingEscrow(_ve).abstain(_tokenId);
     }
@@ -130,7 +151,7 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         uint256 _poolVoteCnt = _poolVote.length;
         uint256 _totalWeight = 0;
 
-        for (uint256 i = 0; i < _poolVoteCnt; i ++) {
+        for (uint256 i = 0; i < _poolVoteCnt; i++) {
             address _pool = _poolVote[i];
             uint256 _votes = votes[_tokenId][_pool];
 
@@ -138,14 +159,16 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
                 weights[_pool] -= _votes;
 
                 votes[_tokenId][_pool] -= _votes;
-                address internal_bribe = gaugeManager.fetchInternalBribeFromPool(_pool);
-                address external_bribe = gaugeManager.fetchExternalBribeFromPool(_pool);
+                address internal_bribe = gaugeManager
+                    .fetchInternalBribeFromPool(_pool);
+                address external_bribe = gaugeManager
+                    .fetchExternalBribeFromPool(_pool);
                 IBribe(internal_bribe).withdraw(uint256(_votes), _tokenId);
                 IBribe(external_bribe).withdraw(uint256(_votes), _tokenId);
 
                 // decrease totalWeight irrespective of gauge is killed/alive for this current pool
                 _totalWeight += _votes;
-                
+
                 emit Abstained(_tokenId, _votes);
             }
         }
@@ -157,39 +180,52 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     /// @notice Recast the saved votes of a given TokenID
     function poke(uint256 _tokenId) external nonReentrant {
         uint256 _timestamp = block.timestamp;
-        if (_timestamp <= HybraTimeLibrary.epochVoteStart(_timestamp)){
+        if (_timestamp <= HybraTimeLibrary.epochVoteStart(_timestamp)) {
             revert("DW");
         }
-        require(IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, _tokenId) || msg.sender == _ve, "NAO||VE");
+        require(
+            IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, _tokenId) ||
+                msg.sender == _ve,
+            "NAO||VE"
+        );
         address[] memory _poolVote = poolVote[_tokenId];
         uint256 _poolCnt = _poolVote.length;
         uint256[] memory _weights = new uint256[](_poolCnt);
 
-        for (uint256 i = 0; i < _poolCnt; i ++) {
+        for (uint256 i = 0; i < _poolCnt; i++) {
             _weights[i] = votes[_tokenId][_poolVote[i]];
-        } 
+        }
 
         _vote(_tokenId, _poolVote, _weights);
     }
 
-    
     /// @notice Vote for pools
     /// @param  _tokenId    veNFT tokenID used to vote
     /// @param  _poolVote   array of LPs addresses to vote  (eg.: [sAMM usdc-usdt   , sAMM busd-usdt, vAMM wbnb-the ,...])
-    /// @param  _weights    array of weights for each LPs   (eg.: [10               , 90            , 45             ,...])  
-    function vote(uint256 _tokenId, address[] calldata _poolVote, uint256[] calldata _weights) 
-        external onlyNewEpoch(_tokenId) nonReentrant {
-        require(IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, _tokenId), "NAO");
+    /// @param  _weights    array of weights for each LPs   (eg.: [10               , 90            , 45             ,...])
+    function vote(
+        uint256 _tokenId,
+        address[] calldata _poolVote,
+        uint256[] calldata _weights
+    ) external onlyNewEpoch(_tokenId) nonReentrant {
+        require(
+            IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, _tokenId),
+            "NAO"
+        );
         require(_poolVote.length == _weights.length, "MISMATCH_LEN");
         require(_poolVote.length <= maxVotingNum, "EXCEEDS");
         uint256 _timestamp = block.timestamp;
-      
+
         _vote(_tokenId, _poolVote, _weights);
         lastVoted[_tokenId] = HybraTimeLibrary.epochStart(block.timestamp) + 1;
         lastVotedTimestamp[_tokenId] = block.timestamp;
     }
-    
-    function _vote(uint256 _tokenId, address[] memory _poolVote, uint256[] memory _weights) internal {
+
+    function _vote(
+        uint256 _tokenId,
+        address[] memory _poolVote,
+        uint256[] memory _weights
+    ) internal {
         _reset(_tokenId);
         uint256 _poolCnt = _poolVote.length;
         uint256 _weight = IVotingEscrow(_ve).balanceOfNFT(_tokenId);
@@ -197,15 +233,15 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         uint256 _usedWeight = 0;
 
         for (uint i = 0; i < _poolCnt; i++) {
-
-            if(gaugeManager.isGaugeAliveForPool(_poolVote[i])) _totalVoteWeight += _weights[i];
+            _totalVoteWeight += _weights[i];
         }
 
         for (uint256 i = 0; i < _poolCnt; i++) {
             address _pool = _poolVote[i];
 
             if (gaugeManager.isGaugeAliveForPool(_pool)) {
-                uint256 _poolWeight = _weights[i] * _weight / _totalVoteWeight;
+                uint256 _poolWeight = (_weights[i] * _weight) /
+                    _totalVoteWeight;
 
                 require(votes[_tokenId][_pool] == 0, "ZV");
                 require(_poolWeight != 0, "ZV");
@@ -214,12 +250,14 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
                 weights[_pool] += _poolWeight;
 
                 votes[_tokenId][_pool] = _poolWeight;
-                address internal_bribe = gaugeManager.fetchInternalBribeFromPool(_pool);
-                address external_bribe = gaugeManager.fetchExternalBribeFromPool(_pool);
-                
+                address internal_bribe = gaugeManager
+                    .fetchInternalBribeFromPool(_pool);
+                address external_bribe = gaugeManager
+                    .fetchExternalBribeFromPool(_pool);
+
                 IBribe(internal_bribe).deposit(uint256(_poolWeight), _tokenId);
                 IBribe(external_bribe).deposit(uint256(_poolWeight), _tokenId);
-                
+
                 _usedWeight += _poolWeight;
                 emit Voted(msg.sender, _tokenId, _poolWeight);
             }
@@ -229,14 +267,15 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         usedWeights[_tokenId] = _usedWeight;
     }
 
-
     modifier onlyNewEpoch(uint256 _tokenId) {
         // ensure new epoch since last vote
-        if (HybraTimeLibrary.epochStart(block.timestamp) <= lastVoted[_tokenId]) revert("VOTED");
-        if (block.timestamp <= HybraTimeLibrary.epochVoteStart(block.timestamp)) revert("DW");
+        if (HybraTimeLibrary.epochStart(block.timestamp) <= lastVoted[_tokenId])
+            revert("VOTED");
+        if (block.timestamp <= HybraTimeLibrary.epochVoteStart(block.timestamp))
+            revert("DW");
         _;
     }
-   
+
     /* -----------------------------------------------------------------------------
     --------------------------------------------------------------------------------
     --------------------------------------------------------------------------------
@@ -251,7 +290,7 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     }
 
     /// @notice view the total length of the voted pools given a tokenId
-    function poolVoteLength(uint256 tokenId) external view returns(uint256) { 
+    function poolVoteLength(uint256 tokenId) external view returns (uint256) {
         return poolVote[tokenId].length;
     }
 
@@ -259,5 +298,4 @@ contract VoterV3 is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         require(_gaugeManager != address(0));
         gaugeManager = IGaugeManager(_gaugeManager);
     }
-    
 }
